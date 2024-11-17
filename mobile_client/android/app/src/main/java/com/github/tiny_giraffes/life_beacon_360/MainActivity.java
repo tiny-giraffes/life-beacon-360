5ebf267b8512a50468913e569ffb8deacb0d0b6e
tyashin <tyashin@gmail.com>
2024-11-17 12:03:15 +0300

    Added Capacitor Android to mobile_client

---
+++ mobile_client/android/app/src/main/java/com/github/tiny_giraffes/life_beacon_360/MainActivity.java
@@ -0,0 +1,5 @@
+package com.github.tiny_giraffes.life_beacon_360;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ mobile_client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.github.tiny_giraffes.life_beacon_360',
+  appName: 'Life beacon 360',
+  webDir: 'dist'
+};
+
+export default config;
